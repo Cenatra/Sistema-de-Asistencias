4ace705e7976ca3157eacce93567a2546c20302b
Cenatra <pablo.ramirezc@salud.gob.mx>
2026-02-24 01:27:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbxVILtgPHuNoCTYXl4eWTCjd8YdzYqIexets92Emve6hz8_qGsc9svBDTIPJQABCLRc/exec";
+
 document.getElementById("loginForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
     const usuario = document.getElementById("usuario").value;
     const password = document.getElementById("password").value;
 
-    if (usuario === "admin" && password === "1234") {
-        alert("Bienvenido Administrador");
-        window.location.href = "dashboard.html"; 
-    } else {
-        alert("Usuario o contraseña incorrectos");
-    }
-});
+    fetch(scriptURL, {
+        method: "POST",
+        body: JSON.stringify({ usuario, password })
+    })
+    .then(res => res.json())
+    .then(response => {
+        if (response.status === "success") {
+            alert("Bienvenido " + response.rol);
+            window.location.href = "dashboard.html";
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("Error de conexión");
+    });
+});;
